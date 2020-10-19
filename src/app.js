d3b368fac29c2f1fbe1dc600b050ff5f4ee5de5b
AnatolijDm <anat_dr25@rambler.ru>
2020-10-19 23:31:48 +0300

    feat: add logs and error handling

--- src/app.js
+++ src/app.js
@@ -5,9 +5,14 @@ const YAML = require('yamljs');
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const { finished } = require('stream');
+const fs = require('fs');
+const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+const logs = path.join(__dirname, 'access.log');
+const errors = path.join(__dirname, 'errors.log');
 
 app.use(express.json());
 
@@ -19,6 +24,67 @@ app.use('/', (req, res, next) => {
     return;
   }
   next();
+});
+
+app.use((req, res, next) => {
+  const { method, url, body } = req;
+  const start = Date.now();
+  finished(res, () => {
+    const ms = Date.now() - start;
+    const { statusCode } = res;
+    const date = new Date(start);
+    const data = `${date} ${method} ${url} ${JSON.stringify(
+      body
+    )} ${statusCode} [${ms}ms]\n`;
+    console.log(data);
+    fs.appendFile(logs, data, err => {
+      if (err) {
+        console.log("Can't input logs to access.log");
+      }
+    });
+  });
+  next();
+  return;
+});
+
+app.use((err, req, res, next) => {
+  const errMessage = res
+    .status(INTERNAL_SERVER_ERROR)
+    .send(getStatusText(INTERNAL_SERVER_ERROR));
+  const start = Date.now();
+  const date = new Date(start);
+  const data = `${errMessage}; ${date} \n`;
+  fs.appendFile(errors, data, error => {
+    if (error) {
+      console.log("Can't input logs to errors.log");
+    }
+  });
+  next(err);
+  return;
+});
+
+process.on('uncaughtException', err => {
+  const start = Date.now();
+  const date = new Date(start);
+  const data = `${err.message}; ${date} \n`;
+  console.error(`captured error: ${data}`);
+  fs.appendFile(errors, data, error => {
+    if (error) {
+      console.log("Can't input logs to errors.log");
+    }
+  });
+});
+
+process.on('unhandledRejection', err => {
+  const start = Date.now();
+  const date = new Date(start);
+  const data = `${err.message}; ${date} \n`;
+  console.error(`captured error: ${data}`);
+  fs.appendFile(errors, data, error => {
+    if (error) {
+      console.log("Can't input logs to errors.log");
+    }
+  });
 });
 
 app.use('/users', userRouter);
